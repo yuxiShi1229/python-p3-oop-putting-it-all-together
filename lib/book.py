8069fb12c14e9bd0acdb110e5e1b8ffba3a7dbf4
yuxi Shi <ys3599@tc.columbia.edu>
2024-07-15 16:06:34 -0400

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count 
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size  
+        self.condition = None
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
